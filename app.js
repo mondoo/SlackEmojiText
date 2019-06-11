580994e7f080a438ecd2626ea44030f321cb1d82
Connor <mondocondo@hotmail.co.uk>
2019-06-11 22:55:14 +0100

    Cleaned up code

---
+++ app.js
@@ -0,0 +1,88 @@
+class SlackRansom {
+    constructor(input, output, characterMap, brands, punctuation) {
+        this.input = input;
+        this.output = output;
+        this.characterMap = characterMap;
+        this.brands = brands;
+        this.punctuation = punctuation;
+        this.bindInput();
+    }
+
+    toEmojis(inStr) {
+        let alphaNumericRegex = /[a-z0-9]/i;
+        let output = "";
+        for (let i = 0; i < inStr.length; i++) {
+            if (this.characterMap[inStr[i].toLowerCase()]) {
+                output += this.characterMap[inStr[i].toLowerCase()];
+            }
+            else if (alphaNumericRegex.test(inStr[i])) {
+                output += `:${inStr[i].toLowerCase()}:`;
+            }
+            else {
+                output += inStr[i];
+            }
+        }
+        return output;
+    }
+
+    validationCheck(inArr) {
+        if (!Array.isArray(inArr)) {
+            return false;
+        }
+        for (let i = 0; i < inArr.length; i++) {
+            if (inArr[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    formatSlackString(inStr) {
+        let output = this.toEmojis(inStr);
+        Object.keys(this.brands).forEach((brand) => {
+            const strToReplace = this.toEmojis(brand);
+            output = output.replace(new RegExp(strToReplace, "g"), (match, idx) => {
+                let punctuationPrevValidation = false;
+                let punctuationNextValidation = false;
+                for (let i = 0; i < this.punctuation.length; i++) {
+                    const punctuationEmoji = this.toEmojis(this.punctuation[i]);
+                    const prevPunctuationSlice = output.slice(idx - punctuationEmoji.length, idx);
+                    const nextPunctuationSlice = output.slice(strToReplace.length + idx, strToReplace.length + idx + punctuationEmoji.length);
+    
+                    if (prevPunctuationSlice == punctuationEmoji) {
+                        punctuationPrevValidation = true;
+                    }
+    
+                    if(nextPunctuationSlice == punctuationEmoji) {
+                        punctuationNextValidation = true;
+                    }
+    
+                    if (punctuationPrevValidation && punctuationNextValidation) {
+                        break;
+                    }
+                }
+    
+                return this.validationCheck([
+                    idx == 0 && !output[strToReplace.length + idx + 1],
+                    punctuationPrevValidation && punctuationNextValidation,
+                    punctuationPrevValidation && (!output[strToReplace.length + idx + 1] || output[strToReplace.length + idx + 1] == " "),
+                    punctuationNextValidation && (!output[idx - 1] || output[idx - 1] == " "),
+                    (!output[idx - 1] || output[idx - 1] == " ") && (!output[strToReplace.length + idx + 1] || output[strToReplace.length + idx + 1] == " ")
+                ]) ? this.brands[brand] : strToReplace;
+            });
+        });
+    
+        return output;
+    }
+
+    bindInput() {
+        let $inputField = document.querySelector(this.input);
+        let $outputField = document.querySelector(this.output);
+    
+        if ($inputField && $outputField) {
+            $inputField.addEventListener('input', (evt) => {
+                $outputField.innerHTML = this.formatSlackString(evt.target.value);
+            });
+        }
+    }
+}
